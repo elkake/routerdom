b7d41a2e6c5aa6195f1816a68b29494742d65ba8
elkake <gianco.marquez@gmail.com>
2022-09-20 12:22:40 -0300

    todo

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/ejemplo4">
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
