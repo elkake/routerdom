7f7f03a1f9d90de5c937f542b57b25c2f7aa3627
elkake <gianco.marquez@gmail.com>
2022-09-20 20:45:25 -0300

    caamibo a hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router basename="ejemplo4">
+  <Router>
     <App />
   </Router>
 );
